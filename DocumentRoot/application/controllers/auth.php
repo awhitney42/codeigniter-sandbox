721bc59f92adca9070bba9fbef3e6283db4f0a0d
Adam Whitney <awhitney@lightedge.com>
2020-02-17 09:59:04 -0600

    Made sure the CodeIgniter application has everything to be able to use the MySQL database if desired.

--- DocumentRoot/application/controllers/auth.php
+++ DocumentRoot/application/controllers/auth.php
@@ -2,62 +2,65 @@
 
 Class Auth extends CI_Controller {
 
-public function __construct() {
-parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-// Load form helper library
-$this->load->helper('form');
+        // Load form helper library
+        $this->load->helper('form');
 
-// Load form validation library
-$this->load->library('form_validation');
+        // Load form validation library
+        $this->load->library('form_validation');
 
-// Load session library
-$this->load->library('session');
-}
-// Show login page
-public function index() {
-$this->load->view('login', array('error_message' => ''));
-}
+        // Load session library
+        $this->load->library('session');
 
-// Check for user login process
-public function user_login_process() {
+        $this->load->database();
 
-$data = array(
-'username' => $this->input->post('username'),
-'password' => $this->input->post('password')
-);
-if ($data['password'] != "ImmaBadHacker") {
-    $result = true;
-} else {
-    $result = false;
-}
-if ($result == TRUE) {
+    }
+    // Show login page
+    public function index() {
+        $this->load->view('login', array('message' => ''));
+    }
 
-$session_data = array(
-'username' => $data['username']
-);
-// Add user data in session
-$this->session->set_userdata('logged_in', $session_data);
-header("Location: " . base_url() . "dashboard");
-} else {
-$data = array(
-'error_message' => 'Invalid Username or Password'
-);
-$this->load->view('login', $data);
-}
-}
+    // Check for user login process
+    public function user_login_process() {
 
-// Logout from admin page
-public function logout() {
+        $data = array(
+        'username' => $this->input->post('username'),
+        'password' => $this->input->post('password')
+        );
+        if ($data['password'] != "ImmaBadHacker") {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        if ($result == TRUE) {
 
-// Removing session data
-$sess_array = array(
-'username' => ''
-);
-$this->session->unset_userdata('logged_in', $sess_array);
-$data['error_message'] = 'Successfully Logout';
-$this->load->view('login', $data);
-}
+            $session_data = array(
+            'username' => $data['username']
+            );
+            // Add user data in session
+            $this->session->set_userdata('logged_in', $session_data);
+            header("Location: " . base_url() . "dashboard");
+        } else {
+            $data = array(
+            'message' => 'Invalid Username or Password'
+            );
+            $this->load->view('login', $data);
+        }
+    }
+
+    // Logout from admin page
+    public function logout() {
+
+        // Removing session data
+        $sess_array = array(
+        'username' => ''
+        );
+        $this->session->unset_userdata('logged_in', $sess_array);
+        $data['message'] = 'Successful Logout';
+        $this->load->view('login', $data);
+    }
 
 }
 
